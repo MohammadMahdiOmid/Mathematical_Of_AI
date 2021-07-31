7b2d3589a4399eb58e4cf24f477e8f841a03ad4e
MohammadMahdiOmid <mohammadmehdiomid@gmail.com>
2021-07-31 19:50:45 +0430

    angle of x and y is computed

--- orthogonality/dot_product.py
+++ orthogonality/dot_product.py
@@ -1,4 +1,5 @@
 import math
+import fractions
 
 
 # simple dot product
@@ -26,8 +27,8 @@ def length(x, y):
     print("Length of y is:", math.sqrt(sum_y))
 
 
+# distance between x and y
 def distance(x, y):
-    print(len(x))
     square = 0
     result = [None] * len(x)
 
@@ -40,10 +41,46 @@ def distance(x, y):
     print("Distance between x and y is:", math.sqrt(square))
 
 
+# angle between x and y
+def angle(x, y):
+    # dot product
+    multiple = 0
+    for i in range(len(x)):
+        multiple = multiple + x[i] * y[i]
+    print('multiple for angle is:', multiple)
+
+    # length of x
+    sum_x = 0
+    for i in range(len(x)):
+        sum_x = sum_x + x[i] * x[i]
+    sum_x = math.sqrt(sum_x)
+
+    print('length x  for angle is:', sum_x)
+
+    # length of y
+    sum_y = 0
+    for i in range(len(y)):
+        sum_y = sum_y + y[i] * y[i]
+    sum_y = math.sqrt(sum_y)
+    print('length y for angle is:', sum_y)
+
+    # multiple of length x and y
+    mul_len = sum_x * sum_y
+    print('multiple of length x and y is :', mul_len)
+
+    # angle of x and y
+    frac = fractions.Fraction(multiple, int(mul_len))
+    print("fraction is:", frac)
+    ang = math.cos(frac)
+
+    print("Angle of x and y is:", ang)
+
+
 # main
 if __name__ == '__main__':
-    x = [1, 2, 3]
-    y = [2, 3, 4]
+    x = [1, 2]
+    y = [2, 1]
     dot(x, y)
     length(x, y)
     distance(x, y)
+    angle(x, y)
