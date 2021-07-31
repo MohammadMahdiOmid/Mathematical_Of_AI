fd76215147a3250c3a17a8389a281ad4875a2d1a
MohammadMahdiOmid <mohammadmehdiomid@gmail.com>
2021-07-31 18:54:54 +0430

    simple dot product is created

--- mean_varianc_covariance/computing.py
+++ mean_varianc_covariance/computing.py
@@ -68,22 +68,6 @@ def computing_covariace():
     print("Automatically covariance with matrix is:",auto_cova)
 
 
-# mutlt_sums = sum_x * sum_y
-# cov = mutlt_sums / len(x)
-# print("covariance is:", cov)
-# print(np.cov(x, y))
-#
-# # Finding the mean of the series x and y
-# mean_x = sum(x) / float(len(x))
-# mean_y = sum(y) / float(len(y))
-# # Subtracting mean from the individual elements
-# sub_x = [i - mean_x for i in x]
-# sub_y = [i - mean_y for i in y]
-# numerator = sum([sub_x[i] * sub_y[i] for i in range(len(sub_x))])
-# denominator = len(x) - 1
-# cov = numerator / denominator
-# print(cov)
-
 if __name__ == '__main__':
 
     number_of_dataset = int(input("Please Enter your count of dataset:"))
---
+++ orthogonality/dot_product.py
@@ -0,0 +1,13 @@
+
+#simple dot product
+def dot(x, y):
+    multiple = 0
+    for i in range(len(x)):
+        multiple = multiple + x[i] * y[i]
+    print(multiple)
+
+
+if __name__ == '__main__':
+    x=[1,2]
+    y=[1,2]
+    dot(x,y)
