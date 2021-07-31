768bcfbef4b1ce3d6a7c3691d1bfb16f4a4025c3
MohammadMahdiOmid <mohammadmehdiomid@gmail.com>
2021-07-31 19:05:09 +0430

    length of x and y is computed.

--- orthogonality/dot_product.py
+++ orthogonality/dot_product.py
@@ -1,13 +1,31 @@
-
-#simple dot product
+import math
+# simple dot product
 def dot(x, y):
     multiple = 0
     for i in range(len(x)):
         multiple = multiple + x[i] * y[i]
-    print(multiple)
+    print("dot product of x and y is:", multiple)
 
-#main
+
+def length(x, y):
+    # length of x
+    sum_x = 0
+    for i in range(len(x)):
+        sum_x = sum_x + x[i] * x[i]
+
+    print("Length of x is:", math.sqrt(sum_x))
+
+    # length of y
+    sum_y = 0
+    for i in range(len(y)):
+        sum_y = sum_y + y[i] * y[i]
+
+    print("Length of y is:", math.sqrt(sum_y))
+
+
+# main
 if __name__ == '__main__':
-    x=[1,2]
-    y=[1,2]
-    dot(x,y)
+    x = [1, 2, 3]
+    y = [2, 3, 4]
+    dot(x, y)
+    length(x, y)
