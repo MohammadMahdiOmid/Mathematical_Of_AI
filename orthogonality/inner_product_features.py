2815e6b2f4a5195f6df0e0327882c071f7e7bf35
MohammadMahdiOmid <mohammadmehdiomid@gmail.com>
2021-08-01 20:12:09 +0430

    Orthogonality is computes

--- orthogonality/inner_product_features.py
+++ orthogonality/inner_product_features.py
@@ -1,4 +1,6 @@
 import math
+import fractions
+
 
 # computing x**T Ay
 def lengthOfVector(x):
@@ -8,6 +10,46 @@ def lengthOfVector(x):
     print("result is:", result)
 
 
+def orthonomial(x, y):
+    # dot product
+    multiple = 0
+    for i in range(len(x)):
+        multiple = multiple + x[i] * y[i]
+    print('multiple for angle is:', multiple)
+
+    # length of x
+    sum_x = 0
+    for i in range(len(x)):
+        sum_x = sum_x + x[i] * x[i]
+    sum_x = math.sqrt(sum_x)
+
+    print('length x  for angle is:', sum_x)
+
+    # length of y
+    sum_y = 0
+    for i in range(len(y)):
+        sum_y = sum_y + y[i] * y[i]
+    sum_y = math.sqrt(sum_y)
+    print('length y for angle is:', sum_y)
+
+    # multiple of length x and y
+    mul_len = sum_x * sum_y
+    print('multiple of length x and y is :', mul_len)
+
+    # angle of x and y
+    frac = fractions.Fraction(multiple, int(mul_len))
+    print("fraction is:", frac)
+    if frac == 0 :
+        print("Two vectores are orthogonal")
+    else:
+        print("Two vectores are not orthogonal")
+    ang = math.cos(frac)
+
+    print("Angle of x and y is:", ang)
+
+
 if __name__ == '__main__':
-    x = [-2, 2]
+    x = [1, 1]
+    y = [-1, 1]
     lengthOfVector(x)
+    orthonomial(x, y)
